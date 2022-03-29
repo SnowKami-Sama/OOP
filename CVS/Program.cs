d33b48ff8c513b62bb44f661b8069c5cdb586af6
SnowKami-Sama <61910822+SnowKami-Sama@users.noreply.github.com>
2022-03-29 08:19:52 +0200

    Figuren en CVS

---
+++ CVS/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace CVS
+{
+
+    class MainClaas
+    {
+        static void Main(string[] args)
+        {
+            Extrastring.CVSinlezenvoorspelers();
+        }
+    }
+
+
+    class Speler
+    {
+
+        private string naam;
+        public string Naam
+        {
+            get { return naam; }
+            set { naam = value; }
+        }
+
+        private string achternaam;
+        public string Achteraam
+        {
+            get { return achternaam; }
+            set { achternaam = value; }
+        }
+
+        private int geboortejaar;
+        public int Geboortejaar
+        {
+            get { return geboortejaar; }
+            set { geboortejaar = value; }
+        }
+
+
+        public Speler(string naam, string achternaam, int geboorterjaar)
+        {
+            this.Naam = naam;
+            this.Achteraam = achternaam;
+            this.Geboortejaar = geboorterjaar;
+        }
+
+    }
+
+
+
+
+    class Extrastring
+    {
+
+        public static void CVSinlezenvoorspelers()
+        {
+            string[] lijnen = File.ReadAllLines(@"C:\Spelers.csv");
+            Speler[] spelers = new Speler[lijnen.Length];
+            for (int i = 0; i < lijnen.Length; i++)
+            {
+                string[] kolomwaarden = lijnen[i].Split(',');
+                spelers[i] = new Speler(kolomwaarden[0], kolomwaarden[1], Convert.ToInt32(kolomwaarden[2]));
+                Console.WriteLine($"{spelers[i].Achteraam} - {spelers[i].Naam} - {spelers[i].Geboortejaar}");
+            }
+
+        }
+
+
+    }
+}
---
+++ Labo 01 Figuren/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Labo_01_Figuren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // DemonstreerFiguren();
+
+
+
+        }
+
+
+        public static void DemonstreerFiguren()
+        {
+            Rechthoek test = new Rechthoek();
+            test.Breedte = -2;
+            test.Hoogte = 4;
+            Console.WriteLine($"Een rechthoek met een breedte van {test.Breedte}m en een hoogte van {test.Hoogte}m heeft een oppervlakte van {test.Oppervlakte}m²");
+        }
+    }
+
+
+    public class Rechthoek
+    {
+
+        private double breedte= 1.0;
+        public double Breedte { get { return breedte; }
+            set {
+                if (value <= 0)
+                {
+                    Console.WriteLine($"Het is verboden een breedte van {value} in te stellen!");
+                }
+                else
+                {
+                    breedte = value;
+                }
+                }
+            }
+
+        private double hoogte = 1.0;
+        public double Hoogte {
+            get { return hoogte; }
+            set
+            {
+                    if (value <= 0)
+                    {
+                        Console.WriteLine($"Het is verboden een hoogte van {value} in te stellen!");
+                    }
+                    else
+                    {
+                        hoogte = value;
+                }
+            }
+
+        }
+
+
+
+        public double Oppervlakte
+        {
+            get { return Math.Round(hoogte * breedte, 2); }
+        }
+
+    }
+
+    public class Driehoek
+    {
+
+        private double basis = 1.0;
+        public double Basis
+        {
+            get { return basis; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine($"Het is verboden een (afmeting) van (waarde) in te stellen!");
+                }
+                else
+                {
+                    basis = value;
+                }
+            }
+        }
+
+        private double hoogte = 1.0;
+        public double Hoogte
+        {
+            get { return hoogte; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine($"Het is verboden een (afmeting) van (waarde) in te stellen!");
+                }
+                else
+                {
+                    hoogte = value;
+                }
+            }
+        }
+
+        public double Oppervlakte
+        {
+            get { return Math.Round((basis * hoogte) / 2.0, 2); }
+        }
+
+    }
+
+
+}
